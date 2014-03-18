76d3e699d417c8e6a832d8565777301700aa248c
rbirger <ruthie.birger@gmail.com>
2014-03-18 14:35:31 -0400

    Basic outline of my modules and functions with some good pep 8 boilerplat

---
+++ coordinates2region.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""This module was created to demonstrate testing at the 3/2014 the NYU
+software carprentry workshop bootcamp. The functions below contain 
+documentation on their use"""
+
+def coordinates2region():
+	"""This is the function we care about"""
+	pass
+	
+def main():
+	"""This is the main controller for my module."""
+	#pass #this is a spot just saying you do nothing
+	#return 'Hi!'
+	print 'Hi!'
+
+
+if __name__ == '__main__': #defines code that will be run when you run this from the command ine
+	main()
+
+#my_hi = main()
+#print my_hi
---
+++ test_coordinates2region.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+"""This is the nosetest module for coordinates2region.py"""
+
+def test_foo():
+	"""Bar"""
+	pass
